f4a40ceeb64c4e5cc45b665fb4eb0bda113a02a5
Vincenzo Buono <vincebshock@gmail.com>
2021-11-05 15:45:58 +0100

    Agent: basic implementation

--- task1/Agent.py
+++ task1/Agent.py
@@ -5,28 +5,62 @@
 # =IMPORTS
 import math
 from libs import vrepConst
+from libs import vrep
+import time
 
 class Agent():
     #########################
     ### Constructor
     #########################
     def __init__(self, client, vrep, actuators, sensors):
-        self.actuators = actuators
-        self.sensors = sensors
-        self.client = client
-        self.vrep = vrep
+        self._actuators = actuators
+        self._sensors = sensors
+        self._client = client
+        self._vrep = vrep
+    
+    #########################
+    ### PROPS
+    #########################
+    ## All props here ##
+    def turnLeft(self, velocity=1, duration=1):
+        start = vrep.simxGetLastCmdTime(self._client)
+        while vrep.simxGetLastCmdTime(self._client) < start + duration:
+            print('turning...')
+            print('time: ', vrep.simxGetLastCmdTime(self._client))
+            self._setMotorSpeed('right', velocity)
+            time.sleep(0.1)
+        #stop all motors
+        print("stopping...")
+        self._stopMotors()
+
+
+    #set motor speed
+    def _setMotorSpeed(self, side='left', velocity=0):
+        #check if side motor has been registered
+        if side+'Motor' not in self._actuators:
+            return -1
+        try:
+            vrep.simxPauseCommunication(self._client,True)
+            vrep.simxSetJointTargetVelocity(self._client, self._actuators[side+'Motor'], velocity, vrepConst.simx_opmode_oneshot )
+        finally:
+            vrep.simxGetPingTime(self._client)
+            self._vrep.simxPauseCommunication(self._client,False)
+    #stop all motors helper function
+    def _stopMotors(self):
+        self._setMotorSpeed('left', 0)
+        self._setMotorSpeed('right', 0)
+
 
     # Given sensor, fetch readings
     # return -> sensors.value if sensor present,
     # otherwise -> -1 
     def getSensorReading(self, sensor):
         print('sensor: ', sensor)
-        print('asens: ', self.sensors)
+        print('Registered Sensors: ', self._sensors)
         #nested method
         def _getObstacleDist(sensorHandler_): #from original implementation
-            print('working')
             # Get raw sensor readings using API
-            rawSR = self.vrep.simxReadProximitySensor(self.client, sensorHandler_, vrepConst.simx_opmode_oneshot_wait)
+            rawSR = self._vrep.simxReadProximitySensor(self._client, sensorHandler_, vrepConst.simx_opmode_oneshot_wait)
             #print('rawsr', rawSR)
             # Calculate Euclidean distance
             if rawSR[1]: # if true, obstacle is within detection range, return distance to obstacle
@@ -35,6 +69,7 @@ class Agent():
                 return float('inf')
 
         #invalid sensor key
-        if sensor not in self.sensors.values():
+        if sensor not in self._sensors.values():
             return -1
         return _getObstacleDist(sensor)
+    
