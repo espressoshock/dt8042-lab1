3bb5a681665d002bad5d279cc0cba5b4a5924a08
Vincenzo Buono <vincebshock@gmail.com>
2021-11-14 21:27:23 +0100

    💄Simulation: console's colors added

--- task2/Simulation.py
+++ task2/Simulation.py
@@ -6,6 +6,7 @@
 from Card import Card
 from Agent import Agent
 from Deck import Deck
+from colorama import Fore, Back, Style, init
 
 
 class Simulation:
@@ -19,6 +20,7 @@ class Simulation:
         self._bid_count = bid_count
         self._player1.set_hands_count(hand_count)
         self._player2.set_hands_count(hand_count)
+        init()  # init colorama
 
     #########################
     ### Start / Game Flow ###
@@ -41,13 +43,9 @@ class Simulation:
             # == Phase 2: Bidding ==
             # ======================
             pot = 0
-            print('bid count ', self._bid_count)
             for _ in range(self._bid_count):
                 pot += self._player1.bid(hand)
                 pot += self._player2.bid(hand)
-
-            print('p1', self._player1.hands[hand].bids)
-            print('p2', self._player2.hands[hand].bids)
 
             # =======================
             # == Phase 3: Showdown ==
@@ -68,29 +66,30 @@ class Simulation:
     ####################
     def show_results(self):
         print(f'Results: ')
-        print(f'Player 1 Statistics:')
+        print(f'{Back.CYAN} Player 1 Statistics: {Style.RESET_ALL}')
         p1_totwinnings = p2_totwinnings = 0
         for i, hand in enumerate(self._player1.hands):
-            print(f'# Hand N.{i}')
-            print(f'\t Cards: {hand.cards}')
-            print(f'\t Bids: {hand.bids}')
-            print(f'\t Winnings: ', end='')
+            print(f'{Back.WHITE}{Fore.BLACK}Hand N.{i}  {Style.RESET_ALL}')
+            print(f'\t {Fore.YELLOW}Cards: {Style.RESET_ALL}{hand.cards}')
+            print(f'\t {Fore.LIGHTRED_EX}Bids: {Style.RESET_ALL}{hand.bids}')
+            print(f'\t {Fore.GREEN}Winnings: {Style.RESET_ALL}', end='')
             for winning in hand.winnings:
                 print(f'{winning}, ', end='')
                 p1_totwinnings += winning
             print(f'\n-----------------------------------')
-        print(f'Player 2 Statistics:')
+        print(f'{Back.MAGENTA} Player 2 Statistics: {Style.RESET_ALL}')
         for i, hand in enumerate(self._player2.hands):
-            print(f'# Hand N.{i}')
-            print(f'\t Cards: {hand.cards}')
-            print(f'\t Bids: {hand.bids}')
-            print(f'\t Winnings: ', end='')
+            print(f'{Back.WHITE}{Fore.BLACK}Hand N.{i}  {Style.RESET_ALL}')
+            print(f'\t {Fore.YELLOW}Cards: {Style.RESET_ALL}{hand.cards}')
+            print(f'\t {Fore.LIGHTRED_EX}Bids: {Style.RESET_ALL}{hand.bids}')
+            print(f'\t {Fore.GREEN}Winnings: {Style.RESET_ALL}', end='')
             for winning in hand.winnings:
                 print(f'{winning}, ', end='')
                 p2_totwinnings += winning
             print(f'\n-----------------------------------')
-        print(f'Player 1 - Total winnings: {p1_totwinnings}')
-        print(f'Player 1 - Total winnings: {p2_totwinnings}')
+        print(f'{Back.CYAN}Player 1 {Style.RESET_ALL} Total winnings: {p1_totwinnings}')
+        print(
+            f'{Back.MAGENTA} Player 2 {Style.RESET_ALL} Total winnings: {p2_totwinnings}')
         print(
             f'Difference: {abs(p1_totwinnings-p2_totwinnings)} in favors of {"Player 1" if p1_totwinnings > p2_totwinnings else "Player 2"}')
 
