48062833fc7a5c42e12528d1f61943f939cdcab4
Vincenzo Buono <vincebshock@gmail.com>
2021-11-14 22:16:20 +0100

    🚀ReflexAgent: added with basic strategy

--- task2/Main.py
+++ task2/Main.py
@@ -7,12 +7,13 @@ from Simulation import Simulation
 from RandomAgent import RandomAgent
 from Card import Card
 from FixedAgent import FixedAgent
+from ReflexAgent import ReflexAgent
 
 
 def main():
     p1 = RandomAgent()
-    p2 = FixedAgent()
-    sim = Simulation(player1=p1, player2=p2, hand_count=1, bid_count=3)
+    p2 = ReflexAgent()
+    sim = Simulation(player1=p1, player2=p2, hand_count=50, bid_count=3)
     sim.start()
     sim.show_results()
 
---
+++ task2/ReflexAgent.py
@@ -0,0 +1,36 @@
+###########################
+### ReflexAgent Class
+##########################
+
+# =IMPORTS
+from Agent import Agent
+
+class ReflexAgent(Agent):
+    #########################
+    ### Constructor
+    #########################
+    def __init__(self, name: str = 'Player 1'):
+        super().__init__(name)
+
+    #########################
+    ### Override Bid
+    #########################
+    def bid(self, hand: int):
+        '''
+        if set([Card('Q', 's'), Card('5', 's'), Card('4', 's')]).issubset(cards):
+            return bid - 10
+        '''
+        # ===================
+        # == Bidded amount ==
+        # ===================
+        bidded = self._hands[hand].bids
+        cards = self._hands[hand].cards
+        #on specific ranks
+        if cards[0] == cards[1] == cards[2]:
+            bid = 50
+        elif cards[0] == cards[1] or cards[0] == cards[2] or cards[1] == cards[2]:
+            bid = 25
+        else:
+            bid = 10
+        self._hands[hand].add_bid(bid)
+        return bid
