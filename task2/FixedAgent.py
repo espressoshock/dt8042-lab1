d6f664c261bbf4159b1115a1720f3433219bd97d
Vincenzo Buono <vincebshock@gmail.com>
2021-11-14 22:12:06 +0100

    🚑️FixedAgent: strategy fixed

--- task2/FixedAgent.py
+++ task2/FixedAgent.py
@@ -18,22 +18,12 @@ class FixedAgent(Agent):
     ### Override Bid
     #########################
     def bid(self, hand: int):
-        #on specific card combinations
-        '''
-        if set([Card('Q', 's'), Card('5', 's'), Card('4', 's')]).issubset(cards):
-            return bid - 10
-        '''
-        # ===================
-        # == Bidded amount ==
-        # ===================
         bidded = self._hands[hand].bids
-        cards = self._hands[hand].cards
-        #on specific ranks
-        if cards[0] == cards[1] == cards[2]:
-            bid = 50
-        elif cards[0] == cards[1] or cards[0] == cards[2] or cards[1] == cards[2]:
-            bid = 25
-        else:
+        if len(bidded) == 0:
             bid = 10
+        if len(bidded) == 1:
+            bid = 25
+        if len(bidded) == 2:
+            bid = 50
         self._hands[hand].add_bid(bid)
         return bid
