5a7f4a053353c0c3fc7582966c2db3b126d1fd00
Vincenzo Buono <vincebshock@gmail.com>
2021-11-16 20:55:41 +0100

    ✨FixedAgent: planned sequential path added

--- task1/FixedAgent.py
+++ task1/FixedAgent.py
@@ -23,13 +23,35 @@ class FixedAgent(Agent):
         self._setTorque(0)
         time.sleep(4)
         self._setTorque(0.5)
-    
+
     def execAndLog(self, logger, action, actionName):
         logger.set_description(
             f'{Back.BLUE} Action {Back.MAGENTA} {actionName}  {Style.RESET_ALL}')
         action
-        logger.update(1)
 
+    # =======================
+    # == Movement Overload ==
+    # =======================
+    def rotateOrthoRightAndLog(self, logger, offset):
+        for i in range(8+offset):
+            self.simulation.collectTargets(False, True)
+            self.execAndLog(logger, self._setMotorSpeed(
+                self.maxSpeed//2, -self.maxSpeed//2), 'driveRotateClockwise')
+            logger.update(1)
+
+    def rotateOrthoLeftAndLog(self, logger, offset):
+        for i in range(8+offset):
+            self.simulation.collectTargets(False, True)
+            self.execAndLog(logger, self._setMotorSpeed(-self.maxSpeed //
+                            2, self.maxSpeed//2), 'driveRotateClockwise')
+            logger.update(1)
+
+    def driveForwardAndLog(self, logger, duration):
+        for i in range(duration):
+            self.simulation.collectTargets(False, True)
+            self.execAndLog(logger, self._setMotorSpeed(
+                self.maxSpeed//2, self.maxSpeed//2), 'driveForward')
+            logger.update(1)
 
     #########################
     ### override
@@ -45,10 +67,44 @@ class FixedAgent(Agent):
         # ======================
         # == Zig-zag strategy ==
         # ======================
-        
-        with tqdm(total=4) as simIters:
-            self.execAndLog(simIters, self.driveRotateClockwise(ROT_SPEED, ORTHO_DT_TICKS), 'driveRotateClockwise')
-            self.execAndLog(simIters, self.driveForward(TRAVELLING_SPEED, 25), 'driveForward')
-            self.execAndLog(simIters, self.driveRotateUnclockwise(ROT_SPEED, ORTHO_DT_TICKS_CORNER), 'driveRotateUnclockwise')
-            self.execAndLog(simIters, self.driveForward(TRAVELLING_SPEED, 50), 'driveForward')
-
+        with tqdm(total=1322) as simIters:
+            actions = [
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 25),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 200),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 10),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 210),
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 10),
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 208),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 10),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 95),
+                self.rotateOrthoRightAndLog(simIters, 0),  # 768 + (72)
+                # around
+                self.driveForwardAndLog(simIters, 30),
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 47),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 62),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 57),
+                self.rotateOrthoLeftAndLog(simIters, 1),
+                self.driveForwardAndLog(simIters, 15),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 47),
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 40),
+                self.rotateOrthoRightAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 53),
+                self.rotateOrthoLeftAndLog(simIters, 0),
+                self.driveForwardAndLog(simIters, 50),  # 1169 + 72 + 72 + 9
+            ]
+            ## Execution action batch
+            for action in actions:
+                action
